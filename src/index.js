e26e5df9bfc6e86f691b1a1975fbf5085e60fe65
Meearlyam <shavela99@gmail.com>
2018-09-23 23:29:41 +0300

    changing index.js file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var love_triangles = 0;
+  for (var i = 0; i < preferences.length; i++){
+    var lover1 = i + 1;
+    var lover2 = preferences[i];
+    var lover3 = preferences[lover2 - 1];
+    var lover = preferences[lover3 - 1];
+    if (lover1 == lover) {
+      love_triangles++;
+    }
+  }
+  return Math.floor(love_triangles/3);
 };
